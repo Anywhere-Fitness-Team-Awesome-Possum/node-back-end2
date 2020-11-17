a0c0863582c21a175b39af0fa4ad6be571ce8f39
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-11-17 16:50:01 -0600

    changed server header

--- api/server.js
+++ api/server.js
@@ -9,18 +9,15 @@ const server = express();
 
 
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 
  
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-
 
 
 server.get('/', (req, res) => {
